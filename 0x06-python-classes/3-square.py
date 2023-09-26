731b97ed88f438b61d91f85d62d1a761b2065e1b
MungaSoftwiz <mwachilumobm@gmail.com>
2023-09-27 01:48:44 +0300

    Define class with a method to find square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A module that defines a class named square"""
+
+
+class Square:
+    """A class that defines a square with validations
+
+    Attributes:
+        __size (int): The private instance attribute
+    """
+
+    def __init__(self, size=0):
+        """The __init__ method initialises instances attribute
+
+        Args:
+            size (int): The size of the square
+
+        Raises:
+            TypeError: Raised if size is not an integer
+            ValueError: Raised if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Method finds the square area
+
+        Returns:
+            the square area of the square
+        """
+        return (self.__size ** 2)
