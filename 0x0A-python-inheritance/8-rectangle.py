adb99a6033206248d6eca1dbcb9c77fad7d56eaa
MungaSoftwiz <mwachilumobm@gmail.com>
2023-11-07 01:31:34 +0300

    Inherit from BaseGeometry class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module contains an  empty class """
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ A child class inherits from BaseGeometry """
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
