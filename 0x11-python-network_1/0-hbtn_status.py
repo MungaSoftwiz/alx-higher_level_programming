b39db428f014515b814295b9916aef0b040bf503
MungaSoftwiz <mwachilumobm@gmail.com>
2024-01-29 00:20:42 +0300

    Fix fetch https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-""" Fetches https://alx-intranet.hbtn.io/status """
+""" The 0-hbtn_status.py module """
+
 import urllib.request
 
 
-if __name__ == 'main':
-    request = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+if __name__ == "__main__":
+    """ Fetches https://alx-intranet.hbtn.io/status """
+
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         body = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(body)))
-        print('\t- content: {}'.format(body))
-        print('\t- utf8 content: {}'.format(body.decode('utf-8')))
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
