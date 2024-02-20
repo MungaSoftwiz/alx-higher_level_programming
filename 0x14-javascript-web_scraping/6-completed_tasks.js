e942fdb93e4307cdd563aa506609a95a13cf52d9
MungaSoftwiz <mwachilumobm@gmail.com>
2024-02-20 23:14:56 +0300

    Compute the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const task of tasks) {
+      if (task.completed) {
+        completed[task.userId] = (completed[task.userId] || 0) + 1;
+      }
+    }
+    console.log(completed);
+  }
+});
